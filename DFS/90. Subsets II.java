0ad252c49069602db905d093c5c064665b2a17ba
unknown <zyi.commerce@outlook.com>
2022-05-10 07:29:32 -0600

    dfs

---
+++ DFS/90. Subsets II.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        dfs(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+    private void dfs(List<List<Integer>> list, ArrayList<Integer> tempList, int[] nums, int start) {
+        list.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1])
+                continue;
+            tempList.add(nums[i]);
+            dfs(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
