8bb841319f2d5ebaa63986bd60da954cd7b09fd8
unknown <zyi.commerce@outlook.com>
2022-05-26 07:50:57 -0600

    BFS Matrix

--- BFS/222. Number of Islands.java
+++ BFS/222. Number of Islands.java
@@ -16,9 +16,15 @@ class Solution {
     }
 
     private void DFSMarking(char[][] grid, int i, int j) {
+        // base case - Check for invalid indices and for sites that are't land
+        // if all surrounding by water or connected land (see code below), then return
         if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1')
             return;
+
+        // mark the site as visited, it is like assume it's now like water
         grid[i][j] = '0';
+
+        // check all adjuacent sites
         DFSMarking(grid, i + 1, j);
         DFSMarking(grid, i - 1, j);
         DFSMarking(grid, i + 1, j + 1);
