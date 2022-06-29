a08f0b7672914de4049347c820ea3e86a96d776e
unknown <zyi.commerce@outlook.com>
2022-06-29 07:30:33 -0600

    Union Find

---
+++ OtherDataStructures/307. Range Sum Query - Mutable.java
@@ -0,0 +1,34 @@
+class NumArray {
+    int[] tree;
+    int n;
+
+    public NumArray(int[] nums) {
+        if (nums.length > 0) {
+            n = nums.length;
+            tree = new int[n * 2];
+            buildTree(nums);
+        }
+    }
+
+    private void buildTree(int[] nums) {
+        for (int i = n,  j = 0; i < 2 * n; i++, j++)
+            tree[i] = nums[j];
+        for (int i = n - 1; i > 0; --i)
+            tree[i] = tree[i * 2] + tree[i * 2 + 1]
+    }
+
+    public void update(int index, int val) {
+
+    }
+
+    public int sumRange(int left, int right) {
+
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
